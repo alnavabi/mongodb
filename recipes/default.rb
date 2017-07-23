c720a51df100f3c03276b086d365ee77eb17a006
alnavabi <al.navabi@gmail.com>
2017-07-23 13:23:38 -0700

    create yum repo file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,23 @@
-#
-# Cookbook:: mongodb
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Configure the package management system (yum).
+# Create a /etc/yum.repos.d/mongodb-org-3.4.repo file so that you can install MongoDB directly, using yum
+# to hold the following configuration information for the MongoDB repository:
+# name=MongoDB Repository
+# baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+# gpgcheck=1
+# enabled=1
+# gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+
+
+
+
+baseurl = "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/"
+template "/etc/yum.repos.d/mongodb-org-3.4.repo"  do
+
+  variables(
+
+    :baseurl => baseurl
+
+  )
+
+end
